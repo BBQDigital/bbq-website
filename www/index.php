52ce2cd5401fa4622bd622bc0cc28b3d25e9d106
Alexander Ward <alex.ward@bbqdigital.com>
2014-11-05 16:25:47 +0000

    Added additional pages plus medified some existing templates

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 <?php perch_layout('global.header'); ?>
     <?php perch_content('landing_section'); ?>
     <div class="main" id="main" role="main">
-        <?php perch_content('parallax_section'); ?>
+        <?php perch_content('header_image_section'); ?>
         <?php perch_content('testimonials_section'); ?>
         <?php perch_content('brands_section'); ?>
     </div>
