51ec38584b1d680b17bd8fac0380ab67515f96c2
Alexander Ward <alex.ward@bbqdigital.com>
2014-11-07 18:57:25 +0000

    added more templates and pages

--- www/perch/templates/pages/generic_content.php
+++ www/perch/templates/pages/generic_content.php
@@ -1,8 +1,7 @@
 <?php include('perch/runtime.php');?>
 <?php perch_layout('global.header'); ?>
-    <?php perch_content('landing_section'); ?>
+<?php perch_content('landing_section'); ?>
     <div class="main" id="main" role="main">
         <?php perch_content('standard_content_section'); ?>
-        <?php perch_content('header_image_section'); ?>
     </div>
 <?php perch_layout('global.footer'); ?>
